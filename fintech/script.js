ccb11269b12603926d84d371026de26d82244979
goho302jo03 <goho302jo03@gmail.com>
2017-03-24 18:14:21 +0800

    Dynamic Wed Finished

--- fintech/script.js
+++ fintech/script.js
@@ -2,4 +2,12 @@ $('.marquee').cycle({
   fx: 'scrollLeft',
   delay: -4000
 });
+/* Open the sidenav */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
+  }
 
+/* Close/hide the sidenav */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
